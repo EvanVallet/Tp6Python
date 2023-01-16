5f6c51ab3cfe19793c656ed837c9dbc473805933
Evan Vallet <evan.vallet@uha.fr>
2023-01-16 08:52:56 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+L=[0]*3
+print("valeur de x : ",L)
+
+print("identifiant de L : ",id(L))
+print("identifiant de L : ",type(L))
+
+print("identifiant de L[0] : ",id(L[0]))
+print("identifiant de L[0] : ",type(L[0]))
+
+print("identifiant de L[1] : ",id(L[1]))
+print("identifiant de L[1] : ",type(L[1]))
+
+print("identifiant de L[2] : ",id(L[2]))
+print("identifiant de L[2] : ",type(L[2]))
+
+L[1] += 1
+
+print("identifiant de L : ",id(L))
+print("identifiant de L : ",type(L))
+
+print("identifiant de L[0] : ",id(L[0]))
+print("identifiant de L[0] : ",type(L[0]))
+
+print("identifiant de L[1] : ",id(L[1]))
+print("identifiant de L[1] : ",type(L[1]))
+
+print("identifiant de L[2] : ",id(L[2]))
+print("identifiant de L[2] : ",type(L[2]))
+
+
